1d730cd3f16f191b0843150ccde03f5d58bb46dd
Laura Patterson <lauraevepatterson@gmail.com>
2024-01-14 14:53:34 -0500

    feat: add Preact component to greet visitors with a randomly-selected welcome message

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
 import { defineConfig } from "astro/config";
+import preact from "@astrojs/preact";
 
+// https://astro.build/config
 export default defineConfig({
-  site: "https://lit-fic-laura.netlify.app/"
+  site: "https://lit-fic-laura.netlify.app/",
+  integrations: [preact()]
 });
